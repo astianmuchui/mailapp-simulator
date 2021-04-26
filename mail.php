81a496a485cab627fdb1785e6d265381a48fe234
Seb Astian <Sebastianmuchui79@gmail.com>
2021-04-26 08:23:56 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,21 +1,22 @@
 <?php
     //Get form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $headers = "MIME-Version: 1.0" ."\r\n";
-    $headers .="Content-Type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= "From: " .$name. "<".$email.">". "\r\n";
-    $toEmail = $_POST['recepient'];
-    $subject = $_POST['subject'];    
-    $body = '
-        <h1>you have received an email from <br> '.$name.'</h1>
-        <h2>'.$email.'</h2>
-        <h3>Message :</h3> <br>
-        <p> '.$message.' </p>
-    ';
+
     //check for submit
     if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $headers = "MIME-Version: 1.0" ."\r\n";
+        $headers .="Content-Type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "From: " .$name. "<".$email.">". "\r\n";
+        $toEmail = $_POST['recepient'];
+        $subject = $_POST['subject'];    
+        $body = '
+            <h1>you have received an email from <br> '.$name.'</h1>
+            <h2>'.$email.'</h2>
+            <h3>Message :</h3> <br>
+            <p> '.$message.' </p>
+        ';
         if(empty($name) && empty($email) && empty($message)){
             return false;
         }else{
